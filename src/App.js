45cf13b64934eca0089dc7afdeac20b7220f61d3
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-07-31 16:01:46 +0200

    updating browsing through app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/login';
 import Register from './components/register';
+import Home from './components/home';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Login/>
-      <Register/>
+       <Router>
+      <Switch>
+        <Route exact path="/" component={Login}></Route>
+        <Route path="/sign-up" component={Register}></Route>
+        <Route path="/home">
+             <Home />
+        </Route>
+      </Switch>
+     
+    </Router>
+
+      <Home/>
+     
     </div>
   );
 }
---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import Login from './login';
+import Register from './register';
+
+function Home(){
+    return (
+        <div>
+             <Login/>
+             <Register/>
+        </div>
+    )
+}
+
+export default Home;
