da8d608a3f1ce4179ca902a23db93f60bb2b8046
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-08-01 21:34:44 +0200

    updated project

--- src/components/login.js
+++ src/components/login.js
@@ -6,8 +6,8 @@ function Login(){
             <div className='img'>
                 <img src="https://images.unsplash.com/photo-1659030639055-9b148cf43731?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
             </div>
-            <div>
-            <h1>Welcome Back</h1>
+            <div className='form'>
+            <h1>Welcome Back!</h1>
             <h2>Manage Your Checklist Easily</h2>
             <label for="email">Email</label><br></br>
             <input type="email" placeholder="Enter your Email here"/><br></br>
--- src/components/register.js
+++ src/components/register.js
@@ -5,7 +5,7 @@ function Register(){
             <div className="img">
                 <img src="https://images.unsplash.com/photo-1659030639055-9b148cf43731?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
             </div>
-            <div>
+            <div className="form">
             <h1>Welcome Back</h1>
             <h2>Manage Your Checklist Easily</h2>
             <label for="name">Email</label><br></br>
